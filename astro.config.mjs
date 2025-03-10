4df9ec49cc0331dd76579499a373e692147337f1
Shoaib-Programmer <shoaibnigam422@gmail.com>
2025-03-10 12:59:12 +0000

    made modifications to make deployable

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,36 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  image: {
+    service: { entrypoint: "astro/assets/services/noop" },
+  },
+  site: "https://Shoaib-Programmer.github.io",
+  base: "/PyChemicals/",
+  integrations: [
+    starlight({
+      title: "PyChemicals",
+      social: {
+        github: "https://github.com/Shoaib-Programmer/PyChemicals",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [{ label: "Example Guide", link: "/guides/example/" }],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: ["./src/styles/globals.css"],
+      defaultLocale: "en",
+      locales: {
+        en: { label: "English" },
+        es: { label: "Español" },
+        fr: { label: "Français" },
+      },
+    }),
+  ],
 });
